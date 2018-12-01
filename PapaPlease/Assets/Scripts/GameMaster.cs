03942de81e5a8b286f44cbc3e47b0852969d2dc2
Ryukkuryuku <ledauphin.luc@gmail.com>
2018-12-01 21:45:07 +0100

    added menu interactoin child

--- PapaPlease/Assets/Scripts/GameMaster.cs
+++ PapaPlease/Assets/Scripts/GameMaster.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class GameMaster : MonoBehaviour {
     public VirtualMother vm;
     public HouseMaster hm;
+    public UIMaster uIMaster;
+    public PlayerBehaviour player;
+
+    static GameMaster _instance;
+
+    public static GameMaster Instance { get { return _instance; } }
 
 	// Use this for initialization
     void Awake ()
     {
+        _instance = this;
         vm.gm = this;
         hm.gm = this;
         vm.Init();
     }
 
-	void Start () {
+    private void OnDestroy()
+    {
+        if (_instance == this)
+            _instance = null;
+    }
+
+    void Start () {
         StartGame();
 	}
 	
---
+++ PapaPlease/Assets/UIMaster.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMaster : MonoBehaviour
+{
+    [SerializeField] GameObject _menuInteractChild;
+    [SerializeField] GameObject _centerCursor;
+
+    private void Start()
+    {
+        ShowOrHideCursor(false);
+    }
+    
+    public bool DisplayMenuInteractChild()
+    {
+        if(_menuInteractChild.gameObject.activeSelf == false)
+        { 
+            _menuInteractChild.gameObject.SetActive(true);
+            ShowOrHideCursor(true);
+            _centerCursor.SetActive(false);
+            GameMaster.Instance.player.GetPlayerMover.FreezeMovement(true);
+            GameMaster.Instance.player.GetPlayerHeadBehaviour.SetFreezeHeadControl(true);
+            return true;
+        }
+        return false;
+    }
+    
+    public void HideMenuInteractChild()
+    {
+        _menuInteractChild.gameObject.SetActive(false);
+        ShowOrHideCursor(false);
+        _centerCursor.SetActive(true);
+        GameMaster.Instance.player.GetPlayerMover.FreezeMovement(false);
+        GameMaster.Instance.player.GetPlayerHeadBehaviour.SetFreezeHeadControl(false);
+    }
+
+    private static void ShowOrHideCursor(bool b)
+    {
+        if (b)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
