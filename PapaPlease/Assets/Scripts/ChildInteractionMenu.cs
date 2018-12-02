d413661a49dc5d6d6af1d5144e278e822e47830e
Ryukkuryuku <ledauphin.luc@gmail.com>
2018-12-02 18:58:52 +0100

    separated interact UI and stats UI

--- PapaPlease/Assets/Scripts/ChildInteractionMenu.cs
+++ PapaPlease/Assets/Scripts/ChildInteractionMenu.cs
@@ -8,24 +8,24 @@ public class ChildInteractionMenu : MonoBehaviour
 
     [SerializeField] ChildOrderButton _childOrderButtonRef;
     [SerializeField] RectTransform childOrderButtonsParent;
-    [SerializeField] Text _childNameText;
+    //[SerializeField] Text _childNameText;
 
     
-    [SerializeField] ChildStatIDsContainer _childStatIdsContainer;
-    [SerializeField] ChildStatGauge _childStatGaugeRef;
-    [SerializeField] ChildStatGauge _childStatSkillRef;
-    [SerializeField] RectTransform _childStatGaugesParent;
+    //[SerializeField] ChildStatIDsContainer _childStatIdsContainer;
+    //[SerializeField] ChildStatGauge _childStatGaugeRef;
+    //[SerializeField] ChildStatGauge _childStatSkillRef;
+    //[SerializeField] RectTransform _childStatGaugesParent;
 
     List<ChildOrderButton> _childOrderButtons;
     List<IPTypeInfo> allIPTypeInfos;
 
-    List<ChildStatGauge> _childStatsGaugesList;
+    //List<ChildStatGauge> _childStatsGaugesList;
 
     ChildCharacter curChild;
 
     private void Awake()
     {
-        _childStatsGaugesList = new List<ChildStatGauge>();
+        //_childStatsGaugesList = new List<ChildStatGauge>();
 
         _childOrderButtons = new List<ChildOrderButton>();
         allIPTypeInfos = GameMaster.Instance.hm.GetAllIPTypeInfos();
@@ -36,54 +36,59 @@ public class ChildInteractionMenu : MonoBehaviour
             _childOrderButtons.Add(childOrderButton);
         }
 
-        foreach (var item in _childStatIdsContainer.GetChildStatIDs)
-        {
-            ChildStatGauge newStatGauge = null;
-            if (item.IsDisplayedAsGauge)
-                newStatGauge = Instantiate(_childStatGaugeRef, _childStatGaugesParent);
-            else
-                newStatGauge = Instantiate(_childStatSkillRef, _childStatGaugesParent);
+        //foreach (var item in _childStatIdsContainer.GetChildStatIDs)
+        //{
+        //    ChildStatGauge newStatGauge = null;
+        //    if (item.IsDisplayedAsGauge)
+        //        newStatGauge = Instantiate(_childStatGaugeRef, _childStatGaugesParent);
+        //    else
+        //        newStatGauge = Instantiate(_childStatSkillRef, _childStatGaugesParent);
 
-            newStatGauge.InitGauge(item);
-            _childStatsGaugesList.Add(newStatGauge);
-        }
+        //    newStatGauge.InitGauge(item);
+        //    _childStatsGaugesList.Add(newStatGauge);
+        //}
     }
 
     public void SetupMenu(ChildCharacter child)
     {
         curChild = child;
-        _childNameText.text = child.childName;
-        RefreshGauges(curChild);
+        //_childNameText.text = child.childName;
+        RefreshButtons(curChild);
     }
 
-    private void RefreshGauges(ChildCharacter child)
+    private void RefreshButtons(ChildCharacter child)
     {
         for (int i = 0; i < allIPTypeInfos.Count; i++)
         {
             _childOrderButtons[i]._button.interactable = allIPTypeInfos[i].isAvailable;
         }
-        foreach (var item in _childStatsGaugesList)
-        {
-            foreach (var curStatInfo in child.statsContainer._childStatInfos)
-            {
-                if (curStatInfo.childStatID == item.GetChildStatID)
-                    item.RefreshGauge(curStatInfo);
-            }
-        }
+        //foreach (var item in _childStatsGaugesList)
+        //{
+        //    foreach (var curStatInfo in child.statsContainer._childStatInfos)
+        //    {
+        //        if (curStatInfo.childStatID == item.GetChildStatID)
+        //            item.RefreshGauge(curStatInfo);
+        //    }
+        //}
+    }
+
+    public void MakeHideMenuInteractChild()
+    {
+        GameMaster.Instance.uIMaster.HideMenuInteractChild();
     }
 
     public void MakeGiveOrder(IPTypeInfo ipTypeInfo)
     {
         Debug.Log("Give Order " + ipTypeInfo.IPType.name + " to " + curChild.childName);
         curChild.GiveOrder(ipTypeInfo.IPType);
+        MakeHideMenuInteractChild();
     }
 
     private void Update()
     {
         if (curChild != null)
         { 
-            RefreshGauges(curChild);
-            Debug.Log("Refresh gauges");
+            RefreshButtons(curChild);
         }
     }
 }
